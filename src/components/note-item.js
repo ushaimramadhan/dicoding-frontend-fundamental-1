4fd5aaa9b0b70b94b63117d674f51bd746869a4f
Ushaim Ramadhan F <adhan1005@gmail.com>
2026-01-12 21:12:52 +0700

    feat: connect data dummy to component

--- src/components/note-item.js
+++ src/components/note-item.js
@@ -1,17 +1,31 @@
 class NoteItem extends HTMLElement {
   constructor() {
     super();
+    this._note = {
+        id: 0,
+        title: "NEED_DATA",
+        body: "NEED_DATA",
+        createdAt: "",
+        archived: false,
+    };
   }
-  connectedCallback() {
+
+  set note(value) {
+    this._note = value;
     this.render();
   }
+
   render() {
     this.innerHTML = `
         <div class="note-item">
-            <h3 class="note-title">Contoh Judul</h3>
-            <p class="note-body">Ini adalah isi catatan contoh.</p>
+            <h3>${this._note.title}</h3>
+            <p>${this._note.body}</p>
         </div>
         `;
+  }
+
+  connectedCallback() {
+    this.render();
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
 import "./components/app-bar.js";
 import "./components/note-item.js";
+import { notesData } from "./notes.js";
+
+const notesListEl = document.getElementById("notes-list");
+
+notesData.forEach((note) => {
+  const noteElement = document.createElement("note-item");
+  noteElement.note = note;
+  notesListEl.appendChild(noteElement);
+});
