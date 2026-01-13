48a0289828e20ff17dbdd44d2ab3a19b9332c27d
Ushaim Ramadhan F <adhan1005@gmail.com>
2026-01-13 17:09:58 +0700

    create CRUD function

--- src/components/note-item.js
+++ src/components/note-item.js
@@ -20,8 +20,19 @@ class NoteItem extends HTMLElement {
         <div class="note-item">
             <h3>${this._note.title}</h3>
             <p>${this._note.body}</p>
+            <button class="btn-delete" type="button">Hapus</button>
         </div>
         `;
+
+    const deleteBtn = this.querySelector(".btn-delete");
+    deleteBtn.addEventListener("click", () => {
+      this.dispatchEvent(
+        new CustomEvent("delete-note", {
+          detail: { id: this._note.id },
+          bubbles: true,
+        })
+      );
+    });
   }
 
   connectedCallback() {
--- src/main.js
+++ src/main.js
@@ -1,24 +1,96 @@
 import "./components/app-bar.js";
 import "./components/note-item.js";
-import { notesData } from "./notes.js";
+// import { notesData } from "./notes.js";
 import "./components/note-input.js";
 import "./style.css";
 
-const notesListEl = document.getElementById("notes-list");
-notesData.forEach((note) => {
-  const noteElement = document.createElement("note-item");
-  noteElement.note = note;
-  notesListEl.appendChild(noteElement);
-});
-
 const noteInputEl = document.querySelector("note-input");
-
 if (noteInputEl) {
-  noteInputEl.addEventListener("submit-note", (event) => {
-    const newNote = event.detail;
-    const noteElement = document.createElement("note-item");
-    noteElement.note = newNote;
-    const notesListEl = document.getElementById("notes-list");
-    notesListEl.appendChild(noteElement);
+  noteInputEl.addEventListener('submit-note', (event) => {
+    const { title, body } = event.detail;
+    const newNote = {
+      title: title, 
+      body: body,
+    };
+    insertNote(newNote);
   });
 }
+
+const BASE_URL = 'https://notes-api.dicoding.dev/v2';
+const getNotes = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/notes`);
+    const responseJson = await response.json();
+    if (responseJson.error) {
+      showResponseMessage(responseJson.message);
+    } else {
+      console.log('DATA BERHASIL DIAMBIL:', responseJson.data);
+      renderNotes(responseJson.data);
+    }
+  } catch (error) {
+    showResponseMessage(error);
+  }
+};
+const showResponseMessage = (message = 'Check your internet connection') => {
+  alert(message);
+};
+getNotes();
+
+const renderNotes = (notes) => {
+  const notesListEl = document.getElementById("notes-list");
+  notesListEl.innerHTML = "";
+
+  notes.forEach((note) => {
+    const noteElement = document.createElement("note-item");
+    noteElement.note = note;
+
+    noteElement.addEventListener('delete-note', (event) => {
+      const noteId = event.detail.id;
+      removeNote(noteId);
+    });
+
+    notesListEl.appendChild(noteElement);
+  });
+};
+
+const insertNote = async (note) => {
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(note),
+    };
+
+    const response = await fetch(`${BASE_URL}/notes`, options);
+    const responseJson = await response.json();
+    if (responseJson.error) {
+      showResponseMessage(error);
+    } else {
+      getNotes();
+    }
+  } catch (error) {
+    showResponseMessage(error);
+  }
+};
+
+const removeNote = async (noteId) => {
+  try {
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await fetch(`${BASE_URL}/notes/${noteId}`, options);
+    const responseJson = await response.json();
+    if (responseJson.error) {
+      showResponseMessage(responseJson.message);
+    } else {
+      getNotes();
+    }
+  } catch (error) {
+    showResponseMessage(error);
+  } 
+};
